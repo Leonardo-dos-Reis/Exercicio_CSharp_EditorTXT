d43dd3abeb77cbe929547299d558f5d74b9351ba
L Reis <leo.reis1802@hotmail.com>
2025-04-19 09:24:37 -0300

    criação do menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Buffers;
+
+namespace TextEitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual operação você deseja realizar?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar um novo arquivo");
+            Console.WriteLine("0 - Para sair do programa");
+
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0 : System.Environment.Exit(0); break;
+                // case 1 : Abrir(); break;
+                // case 2 : Editar(); break;
+                default: Menu(); break;
+            }
+        }
+    }
+}
